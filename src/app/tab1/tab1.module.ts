3a45be70d3a611774926b97c436369aec2d3948b
Venkatesh Potella <venky0895@gmail.com>
2025-05-17 21:16:11 +0530

    Remove - unused component created while creating project

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,10 +1,10 @@
-import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import { IonicModule } from '@ionic/angular';
+
+import { Tab1Page } from './tab1.page';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { AppCurrencyPipe } from '../shared/pipes/app-currency.pipe';
 
@@ -14,7 +14,6 @@ import { AppCurrencyPipe } from '../shared/pipes/app-currency.pipe';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule,
     AppCurrencyPipe
   ],
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,10 +1,10 @@
-import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import { IonicModule } from '@ionic/angular';
+
+import { Tab3Page } from './tab3.page';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
@@ -12,7 +12,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab3PageRoutingModule
   ],
   declarations: [Tab3Page]
